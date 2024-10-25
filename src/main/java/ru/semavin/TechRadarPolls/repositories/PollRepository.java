b66f67cf2fa6e10f955e1a058e731c7a7f350360
asem25 <asemavin250604@gmail.com>
2024-10-25 15:01:39 +0300

    some changes

--- src/main/java/ru/semavin/TechRadarPolls/repositories/PollRepository.java
+++ src/main/java/ru/semavin/TechRadarPolls/repositories/PollRepository.java
@@ -9,19 +9,8 @@ import java.util.List;
 @Repository
 public interface PollRepository extends JpaRepository<Poll, Long> {
 
-    // Получение всех опросов по идентификатору технологии
     List<Poll> findByTechnologyTechId(Long technology_techId);
 
-    // Получение всех опросов по идентификатору технологии и времени опроса
-    List<Poll> findByTechnologyTechIdAndTimeAfter(Long technologyId, LocalDateTime pollTime);
 
-    // Получение последнего опроса по идентификатору технологии после указанного времени
-    Poll findFirstByTechnologyTechIdAndTimeAfterOrderByTimeDesc(Long technologyId, LocalDateTime pollTime);
-
-    // Получение последнего опроса по идентификатору технологии (без времени)
-    Poll findFirstByTechnologyTechIdOrderByTimeDesc(Long technologyId);
-
-    // Получение двух последних опросов по идентификатору технологии
-    List<Poll> findTop2ByTechnologyTechIdOrderByTimeDesc(Long technologyId);
 
 }
